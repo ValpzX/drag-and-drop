408bdd12479ddae5d441989b0f5bc5904539310b
covidfy <vapemo1130@gmail.com>
2023-06-13 14:52:01 -0400

    Adding the drop functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 //variables
 const theButtons = document.querySelectorAll("#buttonHolder img"),
     puzzleBoard = document.querySelector(".puzzle-board"),
-    puzzlePieces = document.querySelectorAll(".puzzle-pieces img");
+    puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
+    dropZones = document.querySelectorAll(".drop-zone");
+
+//store the dragged piece in a globl variable, we will need it in the handleDrop function
+let draggedPiece;
 
 //functions
 function changeBGImage() {
@@ -9,9 +13,23 @@ function changeBGImage() {
 }
 
 function handleStartDrag() {
-    console.log("Started dragging puzzle piece");
+    draggedPiece = this;
+}
+
+function handleDragOver(e) {
+    //this will prevent the default dragover behaviour (e is short for event and can also be passed as "evt" or "event")
+    e.preventDefault();
+}
+
+function handleDrop() {
+    e.preventDefault();
+    console.log("dropped");
+    //this line moves the dragged piece from the left side of the board and drops it into whatever dropzone we choose 
+    this.appendChild(draggedPiece);
 }
 
 //event listeners
 theButtons.forEach(button => button.addEventListener("click", changeBGImage));
 puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
+dropZones.forEach(zone => zone.addEventListener("dragover", handleDragOver));
+dropZones.forEach(zone => zone.addEventListener("drop", handleDrop));
